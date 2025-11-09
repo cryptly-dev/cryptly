4866e574c8082e289592018a5cc4fd78f772f558
b-sw <45360754+b-sw@users.noreply.github.com>
2025-11-09 23:15:33 +0100

    refactor: try different verification

--- backend/src/external-connection/github/core/guards/github-webhook-signature.guard.ts
+++ backend/src/external-connection/github/core/guards/github-webhook-signature.guard.ts
@@ -1,25 +1,40 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { Webhooks } from '@octokit/webhooks';
+import * as crypto from 'crypto';
 import { getEnvConfig } from '../../../../shared/config/env-config';
 
 @Injectable()
 export class GithubWebhookSignatureGuard implements CanActivate {
-  public async canActivate(context: ExecutionContext): Promise<boolean> {
+  private secretToken: string;
+
+  constructor() {
+    this.secretToken = getEnvConfig().github.webhookSecret;
+  }
+
+  public canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    const webhooks = new Webhooks({ secret: getEnvConfig().github.webhookSecret });
 
     const signature = request.headers['x-hub-signature-256'] as string | undefined;
     if (!signature) throw new UnauthorizedException('Missing X-Hub-Signature-256');
 
-    const raw = request.rawBody as Buffer | undefined;
-    if (!raw) throw new UnauthorizedException('Missing rawBody');
-
-    const body = raw.toString('utf8');
-
-    if (!(await webhooks.verify(body, signature))) {
+    if (!this.validatePayload(request.body, signature)) {
       throw new UnauthorizedException('Invalid webhook signature');
     }
 
     return true;
+  }
+
+  private validatePayload(payload: any, signature: string): boolean {
+    const expectedSignature =
+      'sha256=' +
+      crypto.createHmac('sha256', this.secretToken).update(JSON.stringify(payload)).digest('hex');
+
+    if (expectedSignature.length !== signature.length) {
+      return false;
+    }
+
+    return crypto.timingSafeEqual(
+      Buffer.from(expectedSignature, 'ascii'),
+      Buffer.from(signature, 'ascii'),
+    );
   }
 }
